419a4a6ef1366720ba99674ba9fdaa63cb522f32
Nicola Zillio <nicolazillio04@gmail.com>
2025-05-10 19:14:31 +0200

    init

---
+++ src/test/java/com/g63616e617a7a61/nonsensegenerator/AppTest.java
@@ -0,0 +1,20 @@
+package com.g63616e617a7a61.nonsensegenerator;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
